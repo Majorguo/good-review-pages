b13da21c15888669643cc0c02f83c1687ad6ce9b
Majorguo <135103846+Majorguo@users.noreply.github.com>
2025-12-13 22:19:42 +0800

    Update list.js

--- functions/api/list.js
+++ functions/api/list.js
@@ -1,13 +1,20 @@
-export async function onRequestGet({ env }) {
-  const list = await env.REVIEWS_KV.list();
-  const reviews = [];
+export async function onRequest({ env }) {
+  let list = [];
+  let cursor;
+  do {
+    const { keys, list_complete, cursor: nextCursor } = await env.REVIEWS_KV.list({ cursor });
+    cursor = nextCursor;
+    for (const key of keys) {
+      const val = await env.REVIEWS_KV.get(key.name, { type: 'json' });
+      if(val) list.push(val);
+    }
+  } while(cursor);
 
-  for (const key of list.keys) {
-    const item = await env.REVIEWS_KV.get(key.name, { type: 'json' });
-    if (item && !item.deleted) reviews.push(item);
-  }
+  // 过滤已删除并按最新更新时间排序
+  list = list.filter(r => !r.deleted)
+             .sort((a,b) => new Date(b.updated_at) - new Date(a.updated_at));
 
-  return new Response(JSON.stringify(reviews), {
+  return new Response(JSON.stringify(list), {
     headers: { 'Content-Type': 'application/json' }
   });
 }
