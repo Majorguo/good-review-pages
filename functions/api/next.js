ecca9062aad8e82afaf1403a285714ccb1cbc0be
Majorguo <135103846+Majorguo@users.noreply.github.com>
2025-12-13 22:20:26 +0800

    Delete functions/api/next.js

--- functions/api/next.js
+++
@@ -1,4 +0,0 @@
-export async function onRequest({ env }) {
-  const row = await env.DB.prepare("SELECT id,content FROM reviews WHERE deleted=0 ORDER BY id LIMIT 1").first();
-  return Response.json(row || {});
-}
